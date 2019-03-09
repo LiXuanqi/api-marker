644cd55c3865fd2694146f439647b765aeddaaba
lixuanqi <lixuanqi1995@gmail.com>
2019-03-08 21:21:14 -0500

    remove @params, @body, @response annotations and integrate to @api.

--- apimaker/annotations.py
+++ apimaker/annotations.py
@@ -1,45 +1,21 @@
 import functools
 
 
-def api(method, path, desc):
+def api(method, path, desc, params={}, body={}, response={}):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             print("method=%s" % method)
             print("path=%s" % path)
             print("desc=%s" % desc)
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
-
-def params(paramDict):
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            for k, v in paramDict.items():
+            # - params
+            for k, v in params.items():
                 print("%s: %s" % (k, v))
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
-
-def header():
-    pass
-
-def body(bodyDict):
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            for k, v in bodyDict.items():
+            # - body
+            for k, v in body.items():
                 print("%s: %s" % (k, v))
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
-
-def response(responseDict):
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            for k, v in responseDict.items():
+            # - response
+            for k, v in response.items():
                 print("%s: %s" % (k, v))
             return func(*args, **kwargs)
         return wrapper
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,20 +1,25 @@
 from .context import apimaker
 
 
-@apimaker.annotations.api(method='get', path='/courses', desc='api to get all courses')
-@apimaker.annotations.params({
-    'param1': 'number, required',
-    'param2': 'string, required'
-})
-@apimaker.annotations.body({
-    'body1': 'number, required',
-    'body2': 'string, required'
-})
-@apimaker.annotations.response({
-    'response1': 'number',
-    'response2': 'string'
-})
+@apimaker.annotations.api(
+    method='get',
+    path='/courses',
+    desc='api to get all courses',
+    params={
+        'param1': 'number, required',
+        'param2': 'string, required'
+    },
+    body={
+        'body1': 'number, required',
+        'body2': 'string, required'
+    },
+    response={
+        'response1': 'number',
+        'response2': 'string'
+    }
+)
 def getCourses():
     print('return all courses')
 
 getCourses()
+
