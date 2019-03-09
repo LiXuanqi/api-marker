040d29294378663c3d73ca68220e93ed778f69e9
lixuanqi <lixuanqi1995@gmail.com>
2019-03-08 20:42:41 -0500

    add @api annotation.

--- apimaker/annotations.py
+++ apimaker/annotations.py
@@ -1,2 +1,13 @@
-def api():
-    print("I'm an api.")
+import functools
+
+
+def api(method, path, desc):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            print("method=%s" % method)
+            print("path=%s" % path)
+            print("desc=%s" % desc)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,8 @@
 from .context import apimaker
 
-apimaker.annotations.api()
+
+@apimaker.annotations.api(method='get', path='/courses', desc='api to get all courses')
+def getCourses():
+    print('return all courses')
+
+getCourses()
